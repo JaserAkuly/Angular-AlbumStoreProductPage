4d45102772132b444e524589b275a23f78b5d374
JaserAkuly <ja844h@att.com>
2018-12-10 17:09:39 -0600

    displaying a list of products - created product interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,3 +1,4 @@
+import { Product } from './product';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
@@ -9,12 +10,18 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productURL = '../assets/products.json'
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
       <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productURL).map(response =>
+      <Product[]>response.json());
   }
 
 }
